a9baaa181972d0d222863e57a9e96884fc59b490
Taras Moskovych <moskovych.taras@gmail.com>
2018-02-08 13:28:51 +0200

    add homework_06

---
+++ homework_06/js/task1.js
@@ -0,0 +1,21 @@
+var a = +prompt("Input first side of triangle:");
+var b = +prompt("Input second side of triangle:");
+var c = +prompt("Input third side of triangle:");
+
+
+if(isNaN(a) || isNaN(b) || isNaN(c) || a + b <= c || a + c <= b ||  b + c <= a) console.log("Incorrect data");
+else{    
+    var p = (a + b + c) / 2;
+    var s = Math.sqrt(p * (p - a) * (p - b) * (p - c));
+    
+    if((s % 1) != 0) s = s.toFixed(2);
+    console.log("Type of triangle is " + getTypeOfTriangle(a, b, c) + " triangle and square is " + s);
+}
+
+
+function getTypeOfTriangle (a, b, c){ 
+    if((a * a + b * b == c * c) || (a * a + c * c == b * b) || (c * c + b * b == a * a)) return "right";
+    else if(a == b && b == c ) return "equilateral";
+    else if(a == b || a == c || b == c) return "isosceles";
+    else return "scalene";
+}
---
+++ homework_06/js/task2.js
@@ -0,0 +1,27 @@
+var eur = +prompt("Input amount of EURO:");
+var usd = +prompt("Input amount of USD:");
+
+
+if(isNaN(eur) || isNaN(usd) || eur <= 0 || usd <= 0) console.log("Incorrect data");
+else{
+    var eur_uah_rate = 33.4602; // 08.02.18
+    var usd_uah_rate = 27.1196; // 08.02.18
+        
+    var eur_uah_result = checkFloat(eur * eur_uah_rate);
+    var usd_uah_result = checkFloat(usd * usd_uah_rate);
+    var eur_usd_result = checkFloat(eur_uah_rate / usd_uah_rate);
+        
+    var str_eur = " euro is";
+    var str_usd = " dollar is"
+    
+    if(eur > 1) str_eur = " euros are";
+    if(usd > 1) str_usd = " dollars are";
+    
+    console.log(eur +  str_eur + " equal " + eur_uah_result + " UAH, " + usd +  str_usd + " equal " + usd_uah_result + " UAH, one euro is equal " + eur_usd_result + " dollars.");
+}
+
+
+function checkFloat(n){
+    if(n % 1 != 0) return n.toFixed(2);
+    else return n;
+}
