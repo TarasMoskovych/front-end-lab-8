e2c315c0500f50a5d56b8ab243462edfd72adb72
Taras Moskovych <moskovych.taras@gmail.com>
2018-02-10 11:45:58 +0200

    update hw_06

--- homework_06/js/task1.js
+++ homework_06/js/task1.js
@@ -1,6 +1,6 @@
-var a = parseFloat(prompt("Input first side of triangle:"));
-var b = parseFloat(prompt("Input second side of triangle:"));
-var c = parseFloat(prompt("Input third side of triangle:"));
+var a = Number(prompt("Input first side of triangle:"));
+var b = Number(prompt("Input second side of triangle:"));
+var c = Number(prompt("Input third side of triangle:"));
 
 
 if(isNaN(a) || isNaN(b) || isNaN(c) || a + b <= c || a + c <= b ||  b + c <= a){
@@ -9,17 +9,18 @@ if(isNaN(a) || isNaN(b) || isNaN(c) || a + b <= c || a + c <= b ||  b + c <= a){
     var p = (a + b + c) / 2;
     var s = Math.sqrt(p * (p - a) * (p - b) * (p - c));
     
-    if((s % 1) != 0){
-        s = s.toFixed(2); 
-    } 
-    
-    console.log("Type of triangle is " + getTypeOfTriangle(a, b, c) + " triangle and square is " + s);
+    console.log("Type of triangle is " + getTypeOfTriangle(a, b, c) + " triangle and square is " + parseFloat(s.toFixed(2)));
 }
 
 
 function getTypeOfTriangle (a, b, c){ 
-    if((a * a + b * b == c * c) || (a * a + c * c == b * b) || (c * c + b * b == a * a)) return "right";
-    else if(a == b && b == c ) return "equilateral";
-    else if(a == b || a == c || b == c) return "isosceles";
-    else return "scalene";
+    if((a * a + b * b == c * c) || (a * a + c * c == b * b) || (c * c + b * b == a * a)){
+      return "right";  
+    } else if(a == b && b == c ) {
+        return "equilateral";
+    } else if(a == b || a == c || b == c){
+        return "isosceles";
+    } else{
+       return "scalene"; 
+    } 
 }
--- homework_06/js/task2.js
+++ homework_06/js/task2.js
@@ -1,25 +1,18 @@
-var eur = parseFloat(prompt("Input amount of EURO:"));
-var usd = parseFloat(prompt("Input amount of USD:"));
-
+var eur = Number(prompt("Input amount of EURO:"));
+var usd = Number(prompt("Input amount of USD:"));
 
 if(isNaN(eur) || isNaN(usd) || eur <= 0 || usd <= 0){
     console.log("Incorrect data");  
 } else{
-    var eur_uah_rate = 33.4602; // 08.02.18
-    var usd_uah_rate = 27.1196; // 08.02.18
+    var eur_uah_rate = 33.2324; // 10.02.18
+    var usd_uah_rate = 27.1240; // 10.02.18
         
-    var eur_uah_result = checkFloat(eur * eur_uah_rate);
-    var usd_uah_result = checkFloat(usd * usd_uah_rate);
-    var eur_usd_result = checkFloat(eur_uah_rate / usd_uah_rate);
+    var eur_uah_result = parseFloat((eur * eur_uah_rate).toFixed(2));
+    var usd_uah_result = parseFloat((usd * usd_uah_rate).toFixed(2));
+    var eur_usd_result = parseFloat((eur_uah_rate / usd_uah_rate).toFixed(2));
         
     var str_eur = (eur > 1) ? " euros are" : " euro is";
     var str_usd = (usd > 1) ? " dollars are" : " dollar is";
     
     console.log(eur +  str_eur + " equal " + eur_uah_result + " UAH, " + usd +  str_usd + " equal " + usd_uah_result + " UAH, one euro is equal " + eur_usd_result + " dollars.");
-}
-
-
-function checkFloat(n){
-    if(n % 1 != 0) return n.toFixed(2);
-    else return n;
 }
