744c81d26aa145c76bdca71e0db92dda8d052cf3
Taras Moskovych <moskovych.taras@gmail.com>
2018-02-16 17:30:15 +0200

    update reverseNumber

--- homework_08/js/reverseNumber.js
+++ homework_08/js/reverseNumber.js
@@ -1,12 +1,13 @@
 function reverseNumber(inputNumber){
     var reverse = "";
+    var s = 1;
     if(inputNumber < 0){
-        reverse += "-";
-        inputNumber = Math.abs(inputNumber);
+        s = -1;
+        inputNumber *= s;
     }
     while(inputNumber >= 1){
         reverse += parseInt(inputNumber % 10);
         inputNumber = inputNumber / 10;
     }
-    return parseInt(reverse);
+    return reverse * s;
 }
