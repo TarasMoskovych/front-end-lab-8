aad94a191dabac55b0ed534e06d8b899ca2e63b9
Taras Moskovych <moskovych.taras@gmail.com>
2018-02-22 13:12:08 +0200

    fix return in task2

--- homework_10/task2.js
+++ homework_10/task2.js
@@ -44,46 +44,43 @@ function fighter(attacker) {
         hp: attacker.hp
     }
     
-    function getName() {
-        return attacker.name;
-    }
-    
-    function fight(defender) {
-        //player who has 0 hp cannot fight
-        if(stats.hp === 0 || defender.getStats().hp === 0 || attacker === defender){
-            return false;
-        }
-        //defender does not block damage of attacker
-        if(!block()){
-            //if defender's hp = 0, defender lost and fighter won
-            if(defender.getStats().hp - stats.attack <= 0){
-                defender.getCombatHistory().loses++;
-                combatHistory.wins++;
-                defender.getStats().hp = 0; //defender's hp cannot be less than 0
-            } else{
-                defender.getStats().hp -= stats.attack;
+    return{
+        getName: function() {
+            return attacker.name;
+        },
+        
+        fight: function(defender) {
+            //player who has 0 HP cannot fight
+            if(stats.hp === 0 || defender.getStats().hp === 0){
+                return false;
             }
-            return true;
-        } 
-        //defender blocks damage of attacker
-        return false;
-    }
-    
-    function block() {
-        return Math.random() > 0.5;
-        //return false;
-    }
-    
-    function getStats() {
-        return stats;
-    }
-    
-    function getCombatHistory() {
-        return combatHistory;
-    }
-    
-    return { 
-        getName, fight, block, getStats, getCombatHistory
+            //defender did not block damage of attacker
+            if(!defender.block()){
+                //if defender's hp = 0, defender lost and attacker won
+                if(defender.getStats().hp - stats.attack <= 0){
+                    defender.getCombatHistory().loses++;
+                    combatHistory.wins++;
+                    defender.getStats().hp = 0; //defender's hp cannot be less than 0
+                } else{
+                    defender.getStats().hp -= stats.attack;
+                }
+                return true;
+            } 
+            //defender blocked damage of attacker
+            return false;
+        },
+        
+        block: function() {
+            return Math.random() > 0.5;
+        },
+        
+        getStats: function() {
+            return stats;
+        },
+        
+        getCombatHistory: function() {
+            return combatHistory;
+        }   
     }
 }
 
@@ -91,25 +88,9 @@ var fighter1 = fighter({name: 'John', attack: 100, hp: 100});
 var fighter2 = fighter({name: 'Kayn', attack: 50, hp: 300});
 var fighter3 = fighter({name: 'Bill', attack: 40, hp: 100});
 
-fighter1.fight(fighter2); // true, fighter 1 make damage to fighter 2
-fighter1.fight(fighter3); // true, fighter 1 make damage to fighter 3
+fighter1.fight(fighter2); 
+fighter1.fight(fighter3); 
 
-// /**
-//  * Fighter John
-//  * - Combat stats: { wins: 1, loses: 0 }
-//  * - Properties: { name: 'John', attack: 100, hp: 100 }
-//  */
 showResult(fighter1);
-
-// /** Fighter Kayn
-//  * - Combat stats: { wins: 0, loses: 0 }
-//  * - Properties: { name: 'Kayn', attack: 50, hp: 200 }
-//  */
 showResult(fighter2); 
-
-// /**
-//  * Fighter Bill
-//  * - Combat stats: { wins: 0, loses: 1 }
-//  * - Properties: { name: 'Bill', attack: 40, hp: 0 }
-//  */
 showResult(fighter3);
