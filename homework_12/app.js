112fc30aa1cb11ff117daf5b12eeefb95e00336d
Taras Moskovych <moskovych.taras@gmail.com>
2018-02-28 23:08:04 +0200

    implement hash event to switch between pages

--- homework_12/app.js
+++ homework_12/app.js
@@ -1,76 +1,78 @@
-var root = document.getElementById("root");
-var thumbnails = createElement("div", "thumbnails");
+let root = document.getElementById("root");
 
-tankPreview(tanks, thumbnails);
+tankPreview(tanks, root);
+window.onhashchange = hashChange;
 
-function tankPreview(arr, parent){
-    var h1 = createElement("h1", "h1", "Most popular tanks");
-    var div = createElement("div", "flex-container");
+function tankPreview(arr, parent){    
+    parent.innerHTML = "";
     
-    parent.appendChild(h1);
-    parent.appendChild(div);
+    let thumbnails = createElement("div", "thumbnails");
+    let h1 = createElement("h1", "h1", "Most popular tanks");
+    let div = createElement("div", "flex-container");
     
-    for(var i = 0; i < arr.length; i++){
-        var item = createElement("a", "item");
+    thumbnails.appendChild(h1);
+    thumbnails.appendChild(div);
+
+    for(let i = 0; i < arr.length; i++){
+        let item = createElement("a", "item");
         item.setAttribute("title", "Click to details");
-        item.addEventListener("click", openTankDetails);
-        
-        var img = createElement("img");
+        item.addEventListener("click", function(){
+            window.location.hash = arr[i].model;
+        });
+            
+        let img = createElement("img");
         img.setAttribute("src", arr[i].preview);
         
-        var attributes = createElement("div", "tank-attributes");
+        let attributes = createElement("div", "tank-attributes");
+        let container = createElement("div", "country");
         
-        var container = createElement("div", "country");
-        
-        var country = createElement("img");
+        let country = createElement("img");
         country.setAttribute("src", arr[i].country_image);
         country.setAttribute("title", arr[i].country);
         
-        var level = createElement("p");
+        let level = createElement("p");
         level.textContent = arr[i].level;
         
-        var model = createElement("p", "tank-name");
+        let model = createElement("p", "tank-name");
         model.textContent = arr[i].model;
         model.setAttribute("title", arr[i].model);
         
         container.appendChild(country);
-        
         attributes.appendChild(container);  
         attributes.appendChild(level); 
         attributes.appendChild(model);    
-        
         item.appendChild(img);
         item.appendChild(attributes);
-        
         div.appendChild(item);
     }
-    root.appendChild(thumbnails);
+    parent.appendChild(thumbnails);   
 }
 
-
-function tankDetails(tank, arr){
-    var tankDetails = createElement("div", "tank-details");
+function tankDetails2(tank, arr){
+    root.innerHTML = "";
     
-    var div = createElement("div", "flex-container");
+    let tankDetails = createElement("div", "tank-details");
+    let div = createElement("div", "flex-container");
+    let item1 = createElement("div", "item1");
+    let item2 = createElement("div", "item2");
+    let preview = createElement("h2", "h2", "Preview");
+    let characteristics = createElement("h2", "h2", "Characteristics");
     
-    var item1 = createElement("div", "item1");
-    var item2 = createElement("div", "item2");
-    
-    var preview = createElement("h2", "h2", "Preview");
-    var characteristics = createElement("h2", "h2", "Characteristics");
-    
-    var a = createElement("a", "back", "Back to list view");
+    let a = createElement("a", "back", "Back to list view");
     a.setAttribute("href", "#");
+    a.addEventListener("click", function(){
+        location.hash = "";
+    });
 
-    var attributes = createElement("div", "tank-attributes");
-    var container = createElement("div");
-    var country = createElement("img");
-    var level = createElement("p");
-    var model = createElement("p", "tank-name");
-    var img = createElement("img");
-    var table = createElement("table");
+    let attributes = createElement("div", "tank-attributes");
+    let container = createElement("div");
+    let country = createElement("img");
+    let level = createElement("p");
+    let model = createElement("p", "tank-name");
+    let img = createElement("img");
+    let table = createElement("table");
     
-    for(var i = 0; i < arr.length; i++){
+    for(let i = 0; i < arr.length; i++){
         if(tank === arr[i].model){
             country.setAttribute("src", arr[i].country_image);
             country.setAttribute("title", arr[i].country); 
@@ -78,44 +80,46 @@ function tankDetails(tank, arr){
             model.textContent = arr[i].model;
             img.setAttribute("src", arr[i].preview);
             
-            for(var key in arr[i].details){
-                var tr = createElement("tr");
-                var td1 = createElement("td", "td", key);
-                var td2 = createElement("td", "td", arr[i].details[key]);
+            for(let key in arr[i].details){
+                let tr = createElement("tr");
+                let td1 = createElement("td", "td", key);
+                let td2 = createElement("td", "td", arr[i].details[key]);
                 
                 tr.appendChild(td1);
                 tr.appendChild(td2);
-                
                 table.appendChild(tr);
-            } 
+            }
+            
+            item1.appendChild(preview);
+            item1.appendChild(img);
+            item2.appendChild(characteristics);
+            item2.appendChild(table);
+            div.appendChild(item1);
+            div.appendChild(item2);
+            container.appendChild(country);
+            attributes.appendChild(container);  
+            attributes.appendChild(model); 
+            attributes.appendChild(level); 
+            tankDetails.appendChild(attributes);
+            tankDetails.appendChild(div);
+            tankDetails.appendChild(a);
+
+            root.appendChild(tankDetails);
         }
     }
-    item1.appendChild(preview);
-    item1.appendChild(img);
-    
-    item2.appendChild(characteristics);
-    item2.appendChild(table);
-    
-    div.appendChild(item1);
-    div.appendChild(item2);
-    
-    container.appendChild(country);
-        
-    attributes.appendChild(container);  
-    attributes.appendChild(model); 
-    attributes.appendChild(level); 
-    
-    tankDetails.appendChild(attributes);
-    tankDetails.appendChild(div);
-    tankDetails.appendChild(a);
- 
-    root.removeChild(thumbnails);
-    root.appendChild(tankDetails);
 }
 
+function hashChange(){
+    if(location.hash === ""){
+        tankPreview(tanks, root);
+    } else{
+        let name = location.hash;
+        tankDetails2(name.slice(1), tanks);
+    }
+}
 
 function createElement(element, className, textContent){
-    var tmp = document.createElement(element);
+    let tmp = document.createElement(element);
     if(className !== undefined){
         tmp.className = className;
     }
@@ -123,13 +127,4 @@ function createElement(element, className, textContent){
         tmp.textContent = textContent;
     }
     return tmp;
-}
-
-function openTankDetails(){
-    var name = this.getElementsByClassName("tank-name")[0];
-
-    location.assign(location + "#" + name.innerHTML);
-    tankDetails(name.innerHTML, tanks);
-   
-    
 }
