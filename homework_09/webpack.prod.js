991b9ce86fe395edba5ecdd2e25fd7bfbd201eef
Taras Moskovych <moskovych.taras@gmail.com>
2018-04-14 00:29:57 +0300

    config dev and prod

---
+++ homework_09/webpack.prod.js
@@ -0,0 +1,18 @@
+const common = require('./webpack.common.js');
+const merge = require('webpack-merge');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
+module.exports = merge(common, {
+    plugins: [
+        new UglifyJSPlugin(),
+        new OptimizeCssAssetsPlugin({
+            assetNameRegExp: /\.optimize\.css$/g,
+            cssProcessor: require('cssnano'),
+            cssProcessorOptions: { discardComments: { removeAll: true } },
+            canPrint: true
+          }),
+        new BundleAnalyzerPlugin()
+    ]
+});
